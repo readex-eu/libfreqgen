7d14e91381eca4036828f6acb69b7644f8924c55
Robert Schoene <robert.schoene@tu-dresden.de>
2018-02-13 12:59:40 +0100

    comments for sysfs access

--- src/sysfs.c
+++ src/sysfs.c
@@ -22,8 +22,19 @@
 
 static freq_gen_interface_t sysfs_interface;
 
+/* this will contain the start of the sysfs pathes, e.g., /sys/devices/system/cpu/ */
 static char * sysfs_start;
 
+/* will be used during compare */
+struct setting_s
+{
+	int len;
+	char * string;
+};
+
+/*
+ * Tries to locate the sysfs by reading /proc/mounts
+ */
 static int freq_gen_sysfs_init( void )
 {
 	/* check whether the sysfs is mounted */
@@ -61,6 +72,7 @@ static int freq_gen_sysfs_init( void )
 	}
 	endmntent(proc_mounts);
 
+	/*check whether sysfs dir can be opened */
 	DIR * dir = opendir(buffer);
 	if ( dir == NULL )
 	{
@@ -71,15 +83,20 @@ static int freq_gen_sysfs_init( void )
 	{
 		return ENOMEM;
 	}
-
+	/* store sysfs start */
 	sysfs_start = strdup(buffer);
 	return 0;
 
 }
 
+/*
+ * will check whether
+ * /sys/devices/system/cpu/(cpu)/cpufreq/scaling_governor is either userspace, performance or powersave
+ * will check for access to
+ * /sys/devices/system/cpu/(cpu)/cpufreq/scaling_setspeed
+ * */
 static freq_gen_single_device_t freq_gen_sysfs_init_device(int cpu)
 {
-
 	char buffer[BUFFER_SIZE];
 	int fd;
 	if (snprintf(buffer,BUFFER_SIZE,"%scpu%d/cpufreq/scaling_governor",sysfs_start,cpu) == BUFFER_SIZE )
@@ -98,7 +115,13 @@ static freq_gen_single_device_t freq_gen_sysfs_init_device(int cpu)
 	close(fd);
 	if ( strncmp("userspace",buffer,9) != 0)
 	{
-		return -EPERM;
+		if ( strncmp("performance",buffer,9) != 0)
+		{
+			if ( strncmp("powersave",buffer,9) != 0)
+			{
+				return -EPERM;
+			}
+		}
 	}
 
 	if (snprintf(buffer,BUFFER_SIZE,"%scpu%d/cpufreq/scaling_setspeed",sysfs_start,cpu) == BUFFER_SIZE )
@@ -109,6 +132,10 @@ static freq_gen_single_device_t freq_gen_sysfs_init_device(int cpu)
 	return fd;
 }
 
+/*
+ * will return the max nr from /sys/devices/system/cpu/cpu(nr)
+ * will fail on sysfs not accessible
+ * */
 static int freq_gen_sysfs_get_max_sysfs_entries(   )
 {
 	if ( sysfs_start == NULL)
@@ -124,19 +151,23 @@ static int freq_gen_sysfs_get_max_sysfs_entries(   )
 	struct dirent * entry;
 
 	long long int max = 0;
+	/* go through all files/folders under /sys/devices/system/cpu */
 	while ( ( entry = readdir( dir ) ) != NULL )
 	{
+		/* should be a directory */
 		if ( entry->d_type == DT_DIR )
 		{
 			if ( strlen( entry->d_name ) < 4 )
 				continue;
 
+			/* should start with cpu */
 			if ( strncmp(entry->d_name, "cpu", 3 ) == 0 )
 			{
 				/* first after cpu == numerical digit? */
 
 				char* end;
 				long long int current=strtoll(&entry->d_name[3],&end,10);
+				/* should end in an int after cpu */
 				if ( end != ( entry->d_name + strlen(entry->d_name) ) )
 						continue;
 				if ( current > max )
@@ -148,16 +179,10 @@ static int freq_gen_sysfs_get_max_sysfs_entries(   )
 	return max;
 }
 
-struct setting_s
-{
-	int len;
-	char * string;
-};
-
+/* prepares a setting that can be applied */
 static freq_gen_setting_t prepare_sysfs_access(long long int target,int turbo)
 {
 	char buffer[256];
-	sprintf(buffer,"%lli",target/1000);
 	struct setting_s * setting= malloc(sizeof(struct setting_s));
 	if ( setting == NULL)
 	{
@@ -173,6 +198,8 @@ static freq_gen_setting_t prepare_sysfs_access(long long int target,int turbo)
 	return setting;
 }
 
+
+/* free a setting that could be applied */
 static void unprepare_sysfs_access(freq_gen_setting_t setting_in)
 {
 	struct setting_s * setting= (struct setting_s* ) setting_in;
@@ -180,6 +207,9 @@ static void unprepare_sysfs_access(freq_gen_setting_t setting_in)
 	free(setting);
 }
 
+/*
+ * apply a prepared setting for a CPU
+ * */
 static int sysfs_set_frequency(freq_gen_single_device_t fp, freq_gen_setting_t setting_in)
 {
 	struct setting_s * target= (struct setting_s* ) setting_in;

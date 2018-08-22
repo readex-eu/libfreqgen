1d3896961ef0b27c06b572e2382e969aac129000
Robert Schöne <robert.schoene@tu-dresden.de>
2018-08-22 17:53:34 +0200

    fixes

--- include/error.h
+++ include/error.h
@@ -9,9 +9,9 @@
 #define SRC_ERROR_H_
 
 #define LIBFREQGEN_SET_ERROR(...)                                                                  \
-    libfreqgen_set_error_string(__FILE__, __func__, __LINE__, __VA_ARGS__)
+    freq_gen_set_error_string(__FILE__, __func__, __LINE__, __VA_ARGS__)
 #define LIBFREQGEN_APPEND_ERROR(...)                                                               \
-    libfreqgen_append_error_string(__FILE__, __func__, __LINE__, __VA_ARGS__)
+    freq_gen_append_error_string(__FILE__, __func__, __LINE__, __VA_ARGS__)
 
 void freq_gen_set_error_string(const char* error_file, const char* error_func, int error_line,
                                  const char* fmt, ...);
